553b31ea1ec6d2ce9476a4471c4fea47ba28355e
Vladimir888-dot <vmir215@gmail.com>
2024-10-04 08:16:22 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+my_string = input() # Создайте переменную my_string и присвойте ей значение строки с произвольным текстом (функция input()).
+print(len(my_string))    #Выведите на экран длину строки.
+print(my_string.upper()) #Выведите строку my_string в верхнем регистре.
+print(my_string.lower()) #Выведите строку my_string в нижнем регистре.
+my_string_2 = my_string.replace(' ', '') #Измените строку my_string, удалив все пробелы.
+print(my_string_2) # эту строку сделал для себя, чтобы был виден результат
+print(my_string[0]) #Выведите первый символ строки my_string.
+print(my_string[-1]) #Выведите последний символ строки my_string.
